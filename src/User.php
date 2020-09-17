432edd257e3f92add08a528d955fcbb06cd78396
vytexas <vytenis74@gmail.com>
2020-09-17 15:00:48 +0300

    antras

--- src/User.php
+++ src/User.php
@@ -1,29 +1,35 @@
 <?php
-
 namespace userApp;
-
 
 class User
 {
-    public $name;
-    public $email;
-    public $role;
+    public $kodas;
+    public $vardas;
+    public $pavarde;
+    public $pareigos;
+    public $pastas;
+    public $uzmokestis;
+    public $busena;
 
-    public function __construct($name, $email, $role) {
+    public  function  __construct($kodas, $vardas,$pavarde, $pareigos, $pastas, $uzmokestis,$busena){
+        $this->kodas=$kodas;
+        $this->vardas=$vardas;
+        $this->pavarde=$pavarde;
+        $this->pareigos=$pareigos;
+        $this->pastas=$pastas;
+        $this->uzmokestis=$uzmokestis;
+        $this->busena=$busena;
 
-       $this->name = $name;
-       $this->email = $email;
-       $this->role = $role;
     }
-
     public function showProfile() {
-        $data[] = $this->name;
-        $data[] = $this->email;
-        $data[] = $this->role;
+        $data[] = $this->kodas;
+        $data[] = $this->vardas;
+        $data[] = $this->pavarde;
+        $data[] = $this->pareigos;
+        $data[] = $this->pastas;
+        $data[] = $this->uzmokestis;
+        $data[] = $this->busena;
         return $data;
+
     }
 }
-
-
-
-?>
--- view/index.view.php
+++ view/index.view.php
@@ -2,11 +2,61 @@
 
 use userApp\User;
 
-$user = new User("Vytenis", "vytenis@gmail.com", "student");
+for ($i = 0; $i <= 10; $i++){
 
+    $user = new User("123", "Vytenis", "Unkown", "studentas", "vytenis@gmail.com", "500e", "dirbantis" );
 
-foreach ($user->showProfile() as $item) {
-    echo $item . " ";
 }
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #34312d;
+            color: #9fcdff;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Darbuotoju info</h2>
+
+<table>
+    <tr>
+        <th>Asmens kodas</th>
+        <th>Vardas</th>
+        <th>Pavardė</th>
+        <th>Pareigos</th>
+        <th>Paštas</th>
+        <th>Užmokestis</th>
+        <th>Busena</th>
+    </tr>
+    <tr>
+        <?php for ($i=0; $i<=10; $i++) {
+            echo '<tr>';
+            foreach ($user->showProfile() as $item){ ?>
+                <?php echo '<td>'. $item .'</td>';
+            }
+         echo '</tr>';
+        }
+
+        ?>
+    </tr>
+
+</table>
+
+</body>
+</html>
